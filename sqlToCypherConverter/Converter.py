28bb955fc4a3a5cb1984ac8d6a8dbe8cd4f6973b
wtjerry <wtjerry123@gmail.com>
2017-12-16 15:59:17 +0100

    added parameter to convert to specify which tables to convert.

--- sqlToCypherConverter/Converter.py
+++ sqlToCypherConverter/Converter.py
@@ -4,22 +4,30 @@ from sqlToCypherConverter.constants import INSERT_INTO_STATEMENT
 
 class Converter(object):
 
-    def convert(self, sql):
-        with open(sql) as f:
+    def convert(self, sql_file, tables_to_convert):
+        with open(sql_file) as f:
             s = "".join(f.readlines())
-            result = self.to_create_statement(s)
+            result = self.to_create_statement(s, tables_to_convert)
         return "".join(result)
 
-    def to_create_statement(self, s):
+    def to_create_statement(self, s, tables_to_convert):
         result = []
         for sql_statement in s.split(";"):
-            if INSERT_INTO_STATEMENT in sql_statement.lower():
+            if self._is_insertInto(sql_statement) and self._should_convert(sql_statement, tables_to_convert):
                 extractor = Extractor(sql_statement)
                 table_name = extractor.extract_table()
                 columns = extractor.extract_columns()
                 cypher_statement = self._create_statement(table_name, columns)
                 result.append(cypher_statement)
         return "\n".join(result)
+
+    def _is_insertInto(self, sql_statement):
+        return INSERT_INTO_STATEMENT in sql_statement.lower()
+
+    def _should_convert(self, sql_statement, tables_to_convert):
+        if not tables_to_convert:
+            return True
+        return any(x in sql_statement for x in tables_to_convert)
 
     def _create_statement(self, table_name, columns):
         formatted_columns = []
--- tests/ConverterTest.py
+++ tests/ConverterTest.py
@@ -4,7 +4,17 @@ from sqlToCypherConverter.Converter import Converter
 
 
 class ConverterTest(unittest.TestCase):
-    def test_convert(self):
-        result = Converter().convert("./../resources/insertInto.sql")
+    def test_convert_noTablesProvided_convertsEverything(self):
+        result = Converter().convert("./../resources/insertInto.sql", [])
         expected = "CREATE (:STUDENTEN {MatrNr: 24002, Name: 'Xenokrates', Semester: 18})"
+        self.assertEqual(expected, result)
+
+    def test_convert_existingTableProvided_convertsThatTable(self):
+        result = Converter().convert("./../resources/insertInto.sql", ['studenten'])
+        expected = "CREATE (:STUDENTEN {MatrNr: 24002, Name: 'Xenokrates', Semester: 18})"
+        self.assertEqual(expected, result)
+
+    def test_convert_notExistingTableProvided_returnsNothing(self):
+        result = Converter().convert("./../resources/insertInto.sql", ['professoren'])
+        expected = ""
         self.assertEqual(expected, result)
