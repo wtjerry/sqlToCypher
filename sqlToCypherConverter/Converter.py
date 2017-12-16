3ebbe741daf140d75bb9316999b1927e8a147e3a
wtjerry <wtjerry123@gmail.com>
2017-12-16 17:03:38 +0100

    Extracted InsertIntoStatement convertion into its own class.

--- sqlToCypherConverter/Converter.py
+++ sqlToCypherConverter/Converter.py
@@ -1,4 +1,4 @@
-from sqlToCypherConverter.Extractor import Extractor
+from sqlToCypherConverter.InsertIntoStatementConverter import InsertIntoStatementConverter
 from sqlToCypherConverter.constants import INSERT_INTO_STATEMENT
 
 
@@ -6,40 +6,25 @@ class Converter(object):
 
     def convert(self, sql_file, tables_to_convert):
         with open(sql_file) as f:
-            s = "".join(f.readlines())
-            result = self.to_create_statement(s, tables_to_convert)
-        return "".join(result)
+            sql_statements = self._get_sql_statements(f)
+            cypher_statements = self._convert_to_cypher_statements(sql_statements, tables_to_convert)
+        return "\n".join(cypher_statements)
 
-    def to_create_statement(self, s, tables_to_convert):
-        result = []
-        for sql_statement in s.split(";"):
+    def _get_sql_statements(self, f):
+        lines_as_string = "".join(f.readlines())
+        sql_statements = lines_as_string.split(";")
+        return sql_statements
+
+    def _convert_to_cypher_statements(self, sql_statements, tables_to_convert):
+        cypher_statements = []
+        for sql_statement in sql_statements:
             if self._is_insertInto(sql_statement) and self._should_convert(sql_statement, tables_to_convert):
-                extractor = Extractor(sql_statement)
-                table_name = extractor.extract_table()
-                columns = extractor.extract_columns()
-                identifier = self._get_identifier_name(table_name, tables_to_convert)
-                cypher_statement = self._create_statement(table_name, identifier, columns)
-                result.append(cypher_statement)
-        return "\n".join(result)
+                cypher_statement = InsertIntoStatementConverter().to_cypher(sql_statement, tables_to_convert)
+                cypher_statements.append(cypher_statement)
+        return cypher_statements
 
     def _is_insertInto(self, sql_statement):
         return INSERT_INTO_STATEMENT in sql_statement.lower()
 
     def _should_convert(self, sql_statement, tables_to_convert):
         return any(x in sql_statement for x in tables_to_convert)
-
-    def _get_identifier_name(self, table_name, tables_to_convert):
-        return tables_to_convert[table_name]
-
-    def _create_statement(self, table_name, identifier_name, columns):
-        formatted_columns = []
-        for k, v in columns.items():
-            if isinstance(v, str):
-                formatted_columns.append("{0}: '{1}'".format(k, v))
-            else:
-                formatted_columns.append("{0}: {1}".format(k, v))
-
-        identifier_value = columns[identifier_name]
-
-        columns_string = ", ".join(formatted_columns)
-        return "CREATE ({0}:{1} {{{2}}})".format(identifier_value, table_name.upper(), columns_string)
---
+++ sqlToCypherConverter/InsertIntoStatementConverter.py
@@ -0,0 +1,28 @@
+from sqlToCypherConverter.Extractor import Extractor
+
+
+class InsertIntoStatementConverter(object):
+
+    def to_cypher(self, sql_statement, tables_to_convert):
+        extractor = Extractor(sql_statement)
+        table_name = extractor.extract_table()
+        columns = extractor.extract_columns()
+        identifier = self._get_identifier_name(table_name, tables_to_convert)
+        cypher_statement = self._create_statement(table_name, identifier, columns)
+        return cypher_statement
+
+    def _get_identifier_name(self, table_name, tables_to_convert):
+        return tables_to_convert[table_name]
+
+    def _create_statement(self, table_name, identifier_name, columns):
+        formatted_columns = []
+        for k, v in columns.items():
+            if isinstance(v, str):
+                formatted_columns.append("{0}: '{1}'".format(k, v))
+            else:
+                formatted_columns.append("{0}: {1}".format(k, v))
+
+        identifier_value = columns[identifier_name]
+
+        columns_string = ", ".join(formatted_columns)
+        return "CREATE ({0}:{1} {{{2}}})".format(identifier_value, table_name.upper(), columns_string)
