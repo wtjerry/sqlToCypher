43ee992bdb34d3a1651d110d8aa154ba07e7d8c0
wtjerry <wtjerry123@gmail.com>
2017-12-16 18:08:15 +0100

    added hoeren example to main

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from sqlToCypherConverter.Converter import Converter
 
 if __name__ == "__main__":
+    relationship_tables = {
+        'voraussetzen': {
+            'from': "Nachfolger",
+            'to': "Vorgänger"
+        },
+        'hoeren': {
+            'from': "MatrNr",
+            'to': "VorlNr"
+        }
+    }
     tables_to_convert = {
         'studenten': "MatrNr",
         'professoren': "PersNr",
         'assistenten': "PersNr",
         'vorlesungen': "VorlNr",
     }
-    print(Converter().convert("./resources/05 uni-daten.sql", tables_to_convert, {}))
+    print(Converter().convert("./resources/05 uni-daten.sql", tables_to_convert, relationship_tables))
